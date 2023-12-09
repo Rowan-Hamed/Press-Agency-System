fb960c5f8702fa478b1cfce3f182eb44116d40aa
HanyMaged04 <110906579+HanyMaged04@users.noreply.github.com>
2023-12-09 16:15:53 +0200

    Update wall.php

--- View/wall.php
+++ View/wall.php
@@ -1,10 +1,1 @@
-<?php
-    $_SESSION['hh']='gg';
-    include('../model/User.php');
-$hi = new User();
-$hi->login('hany.maged9102@gmail.com','1122');
-    echo $_SESSION['fName'];
-    $hi->setlName('hi');
-    echo $_SESSION['lname'];
-    
-    ?>
+
