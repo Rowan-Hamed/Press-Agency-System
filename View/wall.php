21333e7121ae5bfb11ce99a79e7478fe6c8d5ecc
fady751 <fady.soly55@gmail.com>
2023-12-10 04:17:55 +0200

    .

--- View/wall.php
+++ View/wall.php
@@ -1,1 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/wall_style.css">
+    <title>Press Agency</title>
+</head>
+<body>
+     <header>
+        <div class="logo">Press Agency</div>
+        <div class="toggle"></div>
+        <div class="navigation">
+            <ul>
+                <a href="#index">Home</a>
+                <a href="#login">login</a>
+                <a href="#regeter">register</a>
+                <a href="#viewer">viewer</a>
+                <a href="#admin">admin</a>
+            </ul>
+           
+        </div>
+     </header>
 
+
+      <section class="main">
+        <div>
+            <h2>Hello</h2>
+            <h3>This is Our journalism website</h3>
+            <a href="#admin" class="main-btn"></a>
+            <div class="social-icons">
+            <a href="#"><i class="fa-brands fa-facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-twitter"></i></a>
+            <a href="#"><i class="fa-brands fa-instagram"></i></a>
+            </div>
+        </div>
+        
+      </section>  
+
+    
+    
+</body>
+</html>
