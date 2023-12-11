0f9f097636304643d078a257d6b8071b74faa83c
Hany_Maged <Hany.Maged9102@gmail.com>
2023-12-11 21:37:53 +0200

    add admin

--- model/Admin.php
+++ model/Admin.php
@@ -1,3 +1,80 @@
 <?php
+include ('../model/db/DatabaseClass.php');
+include ('../model/User.php');
+class Admin extends User{
 
+    static function addUser($fName,$lname,$userType,$email,$password,$phoneNum,$urlToPhoto){
+        $sql = "
+        INSERT INTO 'users' ('userType', 'fname', 
+        'lname', 'email', 'phoneNum', 'password', 'urlToPhoto') 
+        VALUES ($userType , $fName , $lname, $email, $phoneNum, $password, $urlToPhoto)
+        ";
+        $res = parent::$userCon->insert($sql);
+        return true;
+    }
+    static function deleteUser($userId){
+        $sql = "DELETE FROM users WHERE id = userId";
+        return parent::$userCon->delete($sql);
+    }
+
+    static function updateUSer($userId,$fName,$lname,$userType,$email,$password,$phoneNum,$urlToPhoto){
+            $sql = "
+                    UPDATE users
+                    SET
+                        fname =      $fName,
+                        lname =      $lname,
+                        email =      $email,
+                        phoneNum =   $phoneNum,
+                        password =   $password,
+                        urlToPhoto = $urlToPhoto
+                    WHERE
+                        id = $userId;
+                ";
+            $res = parent::$userCon->update($sql);
+    }
+
+    static function acceptPost($postId){
+        $sql =
+        "
+        UPDATE post
+        SET
+            status = 1,
+        WHERE
+            postId = $postId;
+        ";
+        parent::$userCon->update($sql);
+    }
+
+    static function refusePost($postId){
+        $sql =
+        "
+        UPDATE post
+        SET
+            status = -1,
+        WHERE
+            postId = $postId;
+        ";
+        parent::$userCon->update($sql);
+    }
+
+    static function deletePost($postId){
+        $sql = "DELETE FROM post WHERE id = $postId";
+        return parent::$userCon->delete($sql);
+    }
+    static function updatePost($postID,$title,$body,$status,$postType,$urlToPhoto){
+        $sql="
+        UPDATE post
+        SET
+            title = $title,
+            body = $body,
+            postType = $postType,
+            status = $status,
+            urlToPhoto = $urlToPhoto,
+        WHERE
+            postId = $postID; 
+        ";
+        parent::$userCon->update($sql);
+    }
+
+}
 ?>
