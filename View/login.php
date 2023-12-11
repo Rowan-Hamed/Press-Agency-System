277b22d30721f3f075aa582d739142ff00df0d18
fady751 <fady.soly55@gmail.com>
2023-12-11 15:46:31 +0200

    .

--- View/login.php
+++ View/login.php
@@ -1,36 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/login.css">
-    <title>login</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      background: url('../assets/photos/style/bg1.jpg') center center fixed; /* Background image */
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      margin: 0;
+    }
+
+    .login-container {
+      background-color: rgba(255, 255, 255, 0.9); /* Background color with opacity */
+      border-radius: 15px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      padding: 20px;
+      text-align: center;
+      max-width: 400px;
+      width: 100%;
+    }
+
+    .login-container h2 {
+      margin-bottom: 20px;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+      text-align: left;
+    }
+
+    .form-group label {
+      display: block;
+      margin-bottom: 5px;
+      font-weight: bold;
+    }
+
+    .form-group input {
+      width: 100%;
+      padding: 10px;
+      box-sizing: border-box;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+    }
+
+    .checkbox-group {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 20px;
+    }
+
+    .checkbox-group label {
+      display: flex;
+      align-items: center;
+    }
+
+    .checkbox-group input {
+      margin-right: 5px;
+    }
+
+    .submit-btn {
+      background-color: #4caf50;
+      color: #fff;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+
+    .submit-btn:hover {
+      background-color: #45a049;
+    }
+
+    .signup-link {
+      margin-top: 10px;
+    }
+  </style>
+  <title>Login Page</title>
 </head>
 <body>
-    <div class="container mt-4" id="login">
-        <h1 class="log">login</h1>
-        <form action="register.html" method="post">
 
-            <div class=" form-control-lg">
-                <label for="username">Username</label>
-                <input type="text" id="username" class="form-control" name="username" placeholder="Username" required>
-            </div>
-
-            <div class="form-control-lg mb-10">
-                <label for="pass">passowrd</label>
-                <input type="password" class="form-control" name="password" id="pass" placeholder="passowrd" required>
-            </div>
-
-            
-            <div class="d-flex justify-content-end pt-3">
-                <button class="btn btn-outline-success">Sign in</button>
-                <button class="btn btn-outline-success ms-2">Sign up</button>
-            </div>
-        </div>
-
-        </form>
+  <div class="login-container">
+    <h2>LOGIN</h2>
     
-    
+    <form>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" required>
+      </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+      </div>
+
+      <div class="checkbox-group">
+        <label>
+          <input type="checkbox" name="remember"> Remember me
+        </label>
+        <a href="#">Forget password?</a>
+      </div>
+
+      <button type="submit" class="submit-btn">Login</button>
+    </form>
+
+    <div class="signup-link">
+      <p>Don't have an account? <a href="#">Sign up</a></p>
+    </div>
+  </div>
+
 </body>
 </html>
