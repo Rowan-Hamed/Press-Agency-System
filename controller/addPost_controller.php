6e3ff353236b400caaa6e9704021ba5e9713c89b
Hany_Maged <Hany.Maged9102@gmail.com>
2023-12-12 21:43:44 +0200

    fixBug

--- controller/addPost_controller.php
+++ controller/addPost_controller.php
@@ -8,13 +8,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $content = $_POST["content"];
     $role = $_POST['Artical-role'];
     $editorId =$_SESSION['id'];
-
+//addPost($title,$body ,$status,$postType,$ownerId,$urlToPhoto){
     $postId = $editor->addPost($title,$content,0,$role,$editorId,'');
     if(isset($_FILES['urlToPhoto']) && !empty($_FILES['urlToPhoto']['name'])) {
         $image_type = strtolower(pathinfo(basename($_FILES['urlToPhoto']["name"]), PATHINFO_EXTENSION));
         $post_image = $postId . '.' . $image_type;
         move_uploaded_file($_FILES["urlToPhoto"]["tmp_name"], "../assets/photos/postPhoto/" . $post_image);
-        $editor->updatePost($postId,$title,$content,0,$role, $post_image);
+        $editor->updatePost($postId,$title,$content,$role, $post_image);
     }
 }
 header("Location:../View/addPost.php");
