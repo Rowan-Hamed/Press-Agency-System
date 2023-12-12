57f88b6143fc233882fccb38356ff2d7c478d685
Fady <Fady.soly55@gmail.com>
2023-12-12 19:52:04 +0200

    .

--- View/test.php
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<form action="../View/editPost.php" method="post">
-    <input type="hidden" name="postId" value="6">
-=======
-<form action="../View/editPost.php" method="get">
-    <input type="hidden" name="postId" value="75">
->>>>>>> 3636965ddc38d745a745dcfd193160b8ffdfb9ec
-    <input type="submit" value="Update Post">
-</form>
