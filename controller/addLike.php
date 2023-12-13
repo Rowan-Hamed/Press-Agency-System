289182791c3c6f3e1cdeae7466ea89fe9a8afe81
Fady <Fady.soly55@gmail.com>
2023-12-13 12:52:30 +0200

    .

--- controller/addLike.php
+++ controller/addLike.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
 
+if(!isset($_SESSION['id'])) {
+    $_SESSION['message_Error'] = "please login first";
+    header('Location: ../view/login.php');
+    exit;
+}
+
+
 require_once ('../model/Viewer.php');
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $viewer = new Viewer();
